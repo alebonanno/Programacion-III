dccba0becdecf681a871282b552fd84e8757e3f0
Mirian Acosta <mirian.net.ar@gmail.com>
2025-08-08 23:33:29 -0300

    Agrego tp1

---
+++ tp1.js
@@ -0,0 +1,15 @@
+//Consigna 1
+let productos = [
+    {Id: "01", nombre: "carpeta", precio: 30000, stock: 10},
+    {Id: "02", nombre: "birome", precio: 4000, stock: 80},
+    {Id: "03", nombre: "goma", precio: 3500, stock: 50},
+    {Id: "04", nombre: "cartuchera", precio: 25000, stock: 7},
+    {Id: "05", nombre: "mochila", precio: 90000, stock: 4},
+]
+
+//Consigna 2
+
+
+//console.log(productos);
+
+
